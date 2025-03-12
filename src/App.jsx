54dc63ab99e59e46dd9583f70ed626e5f1b2c2ce
Sourav Dutta <sourav.equilizer007@gmail.com>
2025-03-13 01:09:25 +0530

    Building more resusable styles components

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,11 @@ import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: aquamarine;
-`;
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.div`
-  background-color: blanchedalmond;
+  /* background-color: blanchedalmond; */
   padding: 15px;
 `;
 
@@ -19,10 +15,37 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <H1>The Wild Oasis</H1>
-        <Button onClick={() => alert("Checked in")}>Check in</Button>
+        {/* Default Row is set to vertical in the Row component */}
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">The Wild Oasis</Heading>
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button
+                variations="primary"
+                sizes="medium"
+                onClick={() => alert("Checked in")}
+              >
+                Check in
+              </Button>
+              <Button
+                variations="secondary"
+                sizes="small"
+                onClick={() => alert("Checked out")}
+              >
+                Check out
+              </Button>
+            </div>
+          </Row>
 
-        <Input type="number" placeholder="No of guests" />
+          <Row type="vertical">
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="No of guests" />
+              <Input type="number" placeholder="No of guests" />
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
