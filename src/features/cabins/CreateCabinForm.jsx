955c6ef638ac48de1fa46c447732bb50bc974787
Sourav Dutta <sourav.equilizer007@gmail.com>
2025-03-16 22:59:45 +0530

    Uploading images

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -28,7 +28,8 @@ function CreateCabinForm() {
   });
 
   function onSubmit(data) {
-    mutate(data);
+    mutate({ ...data, image: data.image[0] });
+    console.log(data);
   }
 
   function onError(error) {
@@ -99,7 +100,11 @@ function CreateCabinForm() {
       </FormRow>
 
       <FormRow lableValue="Cabin photo">
-        <FileInput id="image" accept="image/*" />
+        <FileInput
+          id="image"
+          accept="image/*"
+          {...register("image", { required: "This field is required" })}
+        />
       </FormRow>
 
       <FormRow>
--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,4 +1,5 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
+import { v4 as uuidv4 } from "uuid";
 
 export async function getCabins() {
   const { data: cabins, error } = await supabase.from("cabins").select("*");
@@ -22,10 +23,29 @@ export async function deleteCabins(id) {
 }
 
 export async function createCabin(newCabin) {
-  const { data, error } = await supabase.from("cabins").insert([newCabin]);
+  const imageName = `${uuidv4()}-${newCabin.image.name}`.replaceAll("/", "");
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+
+  //1. Create the cabin
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([{ ...newCabin, image: imagePath }]);
 
   if (error) {
     throw new Error("Cabin cannot be created");
+  }
+
+  //2. Upload Cabin
+  const { error: storageError } = await supabase.storage
+    .from("cabin-images")
+    .upload(imageName, newCabin.image);
+
+  //Delete the Cabin IF there is an error uploading cabin image
+  if (storageError) {
+    await supabase.from("cabins").delete().eq("id", data.id);
+    throw new Error(
+      "Cabin image could not be uploaded and the Cabin was not created"
+    );
   }
 
   return data;
