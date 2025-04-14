f9b5bef01fb42586dfb430c702fec937eab4702a
codexEquilizer <sourav.equilizer007@gmail.com>
2025-04-14 17:32:47 +0530

    Bookings - API side filtering using react-query

--- src/features/bookings/BookingTableOperations.jsx
+++ src/features/bookings/BookingTableOperations.jsx
@@ -3,29 +3,27 @@ import Filter from "../../ui/Filter";
 import TableOperations from "../../ui/TableOperations";
 
 function BookingTableOperations() {
+  const filterOptions = [
+    { value: "all", label: "All" },
+    { value: "checked-out", label: "Checked out" },
+    { value: "checked-in", label: "Checked in" },
+    { value: "unconfirmed", label: "Unconfirmed" },
+  ];
+
+  const sortOptions = [
+    { value: "startDate-desc", label: "Sort by date (recent first)" },
+    { value: "startDate-asc", label: "Sort by date (earlier first)" },
+    {
+      value: "totalPrice-desc",
+      label: "Sort by amount (high first)",
+    },
+    { value: "totalPrice-asc", label: "Sort by amount (low first)" },
+  ];
+
   return (
     <TableOperations>
-      <Filter
-        filterField="status"
-        options={[
-          { value: "all", label: "All" },
-          { value: "checked-out", label: "Checked out" },
-          { value: "checked-in", label: "Checked in" },
-          { value: "unconfirmed", label: "Unconfirmed" },
-        ]}
-      />
-
-      <SortBy
-        options={[
-          { value: "startDate-desc", label: "Sort by date (recent first)" },
-          { value: "startDate-asc", label: "Sort by date (earlier first)" },
-          {
-            value: "totalPrice-desc",
-            label: "Sort by amount (high first)",
-          },
-          { value: "totalPrice-asc", label: "Sort by amount (low first)" },
-        ]}
-      />
+      <Filter filterField="status" options={filterOptions} />
+      <SortBy options={sortOptions} />
     </TableOperations>
   );
 }
